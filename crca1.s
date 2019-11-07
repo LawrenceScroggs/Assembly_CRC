0711f4d55cd149a1c8ff18426e8b04ea61e4cc25
Lawrence Scroggs <lscroggs@augusta.cs.pdx.edu>
2019-11-06 20:25:59 -0800

    starting outline

---
+++ crca1.s
@@ -0,0 +1,39 @@
+# Lawrence Scroggs Homework 2 CS201 10/30/19##############
+#                                                        #
+# In this program I am taking the functions written in C #
+# and converting them into two different Assembly code   #
+# programs.  A practice example where I learn the basics #
+# of Assembly and then an Optimized version of the same  #
+# code to learn how to write the code in a more efficient#
+# way.                                                   #
+#                                                        #
+##########################################################
+
+    .globl crcTable
+
+##########################################################
+    .text
+
+##########################################################
+    .data
+
+
+.rept 256
+.byte 0
+.endr
+
+##########################################################
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
